46764824f60a173e06bf99740d4a69156cb97c8c
Samson Kanthiah Prem Kumar <samson30039@gmail.com>
2026-04-01 19:35:31 +0100

    restarting the development of the app from much cleaner initial commit

--- WorkFlowHub.Api/DTOs/Tasks/UpdateStatusDto.cs
+++
@@ -1,9 +0,0 @@
-using WorkFlowHub.Api.Enums;
-
-namespace WorkFlowHub.Api.DTOs
-{
-    public class UpdateStatusDto
-    {
-        public Status Status { get; set; }
-    }
-}
--- WorkFlowHub.Api/Enums/Status.cs
+++
@@ -1,9 +0,0 @@
-namespace WorkFlowHub.Api.Enums
-{
-    public enum Status
-    {
-        Todo = 0,
-        InProgress = 1,
-        Done = 2
-    }
-}
