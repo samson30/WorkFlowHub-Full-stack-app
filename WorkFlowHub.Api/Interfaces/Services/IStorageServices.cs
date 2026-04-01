d21722361d010e703a972193f5e2aca049b8a8e7
Samson Kanthiah Prem Kumar <samson30039@gmail.com>
2026-04-01 19:35:31 +0100

    restarting the development of the app from much cleaner initial commit

--- WorkFlowHub.Api/Interfaces/Services/IStorageServices.cs
+++
@@ -1,8 +0,0 @@
-namespace WorkFlowHub.Api.Interfaces.Services;
-
-public interface IStorageService
-{
-    Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType);
-    Task<Stream?> DownloadFileAsync(string fileName);
-    Task<bool> DeleteFileAsync(string fileName);
-}
